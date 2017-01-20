e05cc8103ff649c3b96838bb426ba82d54139b2a
Simon Højberg <SimonHoejberg@users.noreply.github.com>
2017-01-20 11:17:26 +0100

    First ish block in mc

---
+++ src/main/java/com/black_dog20/sc/block/CobbleGen.java
@@ -0,0 +1,13 @@
+package com.black_dog20.sc.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class CobbleGen extends Block{
+
+	public CobbleGen(Material materialIn) {
+		super(materialIn);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
--- src/main/java/com/black_dog20/sc/init/ModBlocks.java
+++ src/main/java/com/black_dog20/sc/init/ModBlocks.java
@@ -1,16 +1,22 @@
 package com.black_dog20.sc.init;
 
+import com.black_dog20.sc.block.CobbleGen;
 import com.black_dog20.sc.creativetab.CreativeTabSC;
 import com.black_dog20.sc.reference.Reference;
 
+import net.minecraft.block.material.Material;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks {
 
 	//public static final BlockAncientTable ancientTable = new BlockAncientTable();
-
+	
+	public static final CobbleGen cobbleGen = new CobbleGen(Material.ROCK);
+	
+	@SuppressWarnings("deprecation")
 	public static void init() {
 		//GameRegistry.registerBlock(ancientTable, "ancientTable");
+		GameRegistry.registerBlock(cobbleGen, "cobbleGen");
 	}
 }

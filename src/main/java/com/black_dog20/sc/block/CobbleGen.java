02a546d01becd6f05fb289bee64469a114d3dad3
Simon Højberg <SimonHoejberg@users.noreply.github.com>
2017-01-20 12:15:22 +0100

    Made a block that actually gives the player cobble stone

--- src/main/java/com/black_dog20/sc/block/CobbleGen.java
+++ src/main/java/com/black_dog20/sc/block/CobbleGen.java
@@ -2,12 +2,32 @@ package com.black_dog20.sc.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class CobbleGen extends BlockSC{
 
 	public CobbleGen(Material materialIn) {
 		super("cobbleGen",materialIn);
-		// TODO Auto-generated constructor stub
+		
 	}
 	
+	
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
+				   EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
+			playerIn.inventory.addItemStackToInventory(new ItemStack(Blocks.COBBLESTONE, 1));
+			
+		return true;
+	}
 }
